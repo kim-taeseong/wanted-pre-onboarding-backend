b53a6db6e824cfe831cf9e74e36baaa94cae20ce
taeseong <dnfrkwhr2424@naver.com>
2023-10-03 19:07:06 +0900

    feat(models): 세 개의 모델 생성

--- intern_project/job/models.py
+++ intern_project/job/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+
+class Job(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    position = models.CharField(max_length=100)
+    reward = models.PositiveIntegerField()
+    content = models.TextField()
+    technology = models.CharField(max_length=100)
+
+class Applying(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
