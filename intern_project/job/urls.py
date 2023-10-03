86253f023c9e058b20ad40019371cf58acdbbcde
taeseong <dnfrkwhr2424@naver.com>
2023-10-03 22:55:58 +0900

    feat(csrf): csrf cookie 세팅 추가

--- intern_project/job/urls.py
+++ intern_project/job/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('job/', views.job, name='job')
+    path('job/', views.job, name='job'),
+    path('cookie/', views.csrf_cookie, name='csrf_cookie'),
 ]
--- intern_project/job/views.py
+++ intern_project/job/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 from django.http import JsonResponse
+
+from django.views.decorators.csrf import ensure_csrf_cookie
 # Create your views here.
 
 def job(request):
     return JsonResponse({'data': 'Ok'})
+
+@ensure_csrf_cookie
+def csrf_cookie(request):
+    msg = {'data': 'csrf cookie created'}
+    return JsonResponse(msg)
