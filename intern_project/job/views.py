6ecee51ed72681539b3353608e2c729577fe683a
taeseong <dnfrkwhr2424@naver.com>
2023-10-04 15:30:10 +0900

    feat(post): 채용공고 등록

--- intern_project/job/views.py
+++ intern_project/job/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-
 from django.http import JsonResponse
-
 from django.views.decorators.csrf import ensure_csrf_cookie
+
+from .models import *
 # Create your views here.
 
 def job(request):
-    return JsonResponse({'data': 'Ok'})
+    print(request)
+    if request.method == 'POST':
+        company_id = request.POST.get('company_id')
+        position = request.POST.get('position')
+        reward = request.POST.get('reward')
+        content = request.POST.get('content')
+        technology = request.POST.get('technology')
+
+        job = Job(company_id=company_id, position=position, reward=reward, content=content, technology=technology)
+        job.save()
+        return JsonResponse({'data': 'job posted!'}, status=201)
 
 @ensure_csrf_cookie
 def csrf_cookie(request):
