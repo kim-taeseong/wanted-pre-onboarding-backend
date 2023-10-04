e369d10178685dcd0507335f41bb4250f417283b
taeseong <dnfrkwhr2424@naver.com>
2023-10-04 17:05:55 +0900

    style(put): 이름 수정, if 추가
    - 다른 요청을 위해 이름 수정
    - 요청에 따라 다른 코드 작성을 위해 if 추가

--- intern_project/job/urls.py
+++ intern_project/job/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('job/', views.create_job, name='create_job'),
-    path('job/<int:id>/', views.update_job, name='update_job'),
+    path('job/<int:id>/', views.job_id, name='job_id'),
     path('cookie/', views.csrf_cookie, name='csrf_cookie'),
 ]
--- intern_project/job/views.py
+++ intern_project/job/views.py
@@ -18,19 +18,20 @@ def create_job(request):
     job.save()
     return JsonResponse({'data': 'job posted!'}, status=201)
 
-def update_job(request, id):
-    job = Job.objects.get(id=id)
+def job_id(request, id):
+    if request.method == 'PUT':
+        job = Job.objects.get(id=id)
 
-    data = json.loads(request.body.decode('utf-8'))
+        data = json.loads(request.body.decode('utf-8'))
 
-    job.position = data.get('채용포지션')
-    job.reward = data.get('채용보상금')
-    job.content = data.get('채용내용')
-    job.technology = data.get('사용기술')
+        job.position = data.get('채용포지션')
+        job.reward = data.get('채용보상금')
+        job.content = data.get('채용내용')
+        job.technology = data.get('사용기술')
 
-    job.save()
+        job.save()
 
-    return JsonResponse({'data': 'job updated!'}, status=200)
+        return JsonResponse({'data': 'job updated!'}, status=200)
 
 @ensure_csrf_cookie
 def csrf_cookie(request):
