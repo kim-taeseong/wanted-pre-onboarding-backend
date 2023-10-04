96ed933d26d81b7d6e767e79a3836051099a1c57
taeseong <dnfrkwhr2424@naver.com>
2023-10-04 15:27:36 +0900

    feat(admin): 테이블 컬럼 표시

--- intern_project/job/admin.py
+++ intern_project/job/admin.py
@@ -3,6 +3,19 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Job)
-admin.site.register(Company)
-admin.site.register(Applying)
+# admin.site.register(Job)
+# admin.site.register(Company)
+# admin.site.register(Applying)
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'country', 'region']
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['id', 'company_id', 'position', 'reward', 'content', 'technology']
+
+@admin.register(Applying)
+class ApplyingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'job_id', 'user_id']
