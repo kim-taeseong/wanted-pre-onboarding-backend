6399e4a817ee305f4452941e8011f35f027deb5c
taeseong <dnfrkwhr2424@naver.com>
2023-10-04 16:44:05 +0900

    feat(put): 채용공고 수정

--- intern_project/job/urls.py
+++ intern_project/job/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('job/', views.create_job, name='create_job'),
+    path('job/<int:id>/', views.update_job, name='update_job'),
     path('cookie/', views.csrf_cookie, name='csrf_cookie'),
 ]
--- intern_project/job/views.py
+++ intern_project/job/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 from .models import *
+
+import json
 # Create your views here.
 
 def create_job(request):
@@ -15,6 +17,20 @@ def create_job(request):
     job = Job(company_id=company_id, position=position, reward=reward, content=content, technology=technology)
     job.save()
     return JsonResponse({'data': 'job posted!'}, status=201)
+
+def update_job(request, id):
+    job = Job.objects.get(id=id)
+
+    data = json.loads(request.body.decode('utf-8'))
+
+    job.position = data.get('채용포지션')
+    job.reward = data.get('채용보상금')
+    job.content = data.get('채용내용')
+    job.technology = data.get('사용기술')
+
+    job.save()
+
+    return JsonResponse({'data': 'job updated!'}, status=200)
 
 @ensure_csrf_cookie
 def csrf_cookie(request):
